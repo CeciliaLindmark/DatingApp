60870f8ce8f7a01f0ee9eacacbdacfdd0a40bff4
Cecilia Lindmark <cecilia.lindmark@gmail.com>
2020-01-05 12:35:05 +0100

    Just small update

--- DatingApp.API/Models/Value.cs
+++ DatingApp.API/Models/Value.cs
@@ -1,3 +1,11 @@
+/*Class: Value
+A class used to store values
+Version: 1.0
+Author: Cecilia Lindmark
+Date: 20200105
+PART OF UDEMY COURSE
+*/
+
 namespace DatingApp.API.Models
 {
     public class Value
